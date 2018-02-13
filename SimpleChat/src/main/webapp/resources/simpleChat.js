deb90b9bf176eaad33e95ae4feeb3febea549a2b
jarm14 <joelram1@hotmail.com>
2018-02-13 09:55:31 -0500

    Corrección salto de linea
    Corrección en salto de linea para envio y llegada de mensajes

--- SimpleChat/src/main/webapp/resources/simpleChat.js
+++ SimpleChat/src/main/webapp/resources/simpleChat.js
@@ -1,44 +1,44 @@
 var websocket;
 
 window.onload = function () {
-  invokeConnection();
+    invokeConnection();
 }
 
 function invokeConnection() {
-  websocket = new WebSocket(obtainUri());
-  websocket.onerror = function (evt) {
-    onError(evt)
-  };
-  websocket.onmessage = function (evt) {
-    onMessage(evt)
-  };
-  return true;
+    websocket = new WebSocket(obtainUri());
+    websocket.onerror = function (evt) {
+        onError(evt)
+    };
+    websocket.onmessage = function (evt) {
+        onMessage(evt)
+    };
+    return true;
 }
 
 function obtainUri() {
-  return "ws://localhost:8877/ws";
+    return "ws://localhost:8877/ws";
 }
 
 function onError(evt) {
-  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
 }
 
 function onMessage(evt) {
-  element = document.getElementById("mensajes");
-  if (element.value.length === 0) {
-    element.value = evt.data;
-  } else {
-    oldTexts = element.value.split("\n").slice(-19);
-    element.value = oldTexts.join("\n") + evt.data;
-    element.scrollTop = element.scrollHeight;
-  }
-  return;
+    element = document.getElementById("mensajes");
+    if (element.value.length === 0) {
+        element.value = evt.data + "\n";
+    } else {
+        oldTexts = element.value.split("\n").slice(-19);
+        element.value = oldTexts.join("\n") + evt.data;
+        element.scrollTop = element.scrollHeight;
+    }
+    return;
 }
 
 function acceptValue(element) {
-  websocket.send(element.value);
-  element.value = "";
-  return true;
+    websocket.send(element.value + "\n");
+    element.value = "";
+    return true;
 }
 
 
